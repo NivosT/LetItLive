005aa97419c90c6db482b9a7a52cb2b497427bfa
Nminsker <58308994+Nminsker@users.noreply.github.com>
2021-07-11 18:22:36 +0300

    Fixed timing
    The code now is implemented using a timer library to call the sample thingy every 1 hour.

--- LetItLive_jul07a.ino
+++ LetItLive_jul07a.ino
@@ -1,7 +1,7 @@
 #include "arduino_secrets.h"
 #include "thingProperties.h"
 #include <Arduino_MKRIoTCarrier.h>
-#include <TimeLib.h>
+#include <arduino-timer.h>
 
 #define AirValue 1023
 #define MoisterValue 700
@@ -24,16 +24,16 @@ const int LOVES_WET_SOIL = 16;
 const int LOVES_MOIST_SOIL = 32;
 const int LOVES_DRY = 64;
 
+auto timer = timer_create_default();
 
-
-//defines the globals with the min/max values for moister & light
+//define the globals with the min/max values for moister & light
 int minLight, maxLight, minMoist, maxMoist;
+
 
 void setup() 
 {
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
-
   // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
   delay(1500);
   while (!Serial);
@@ -56,16 +56,57 @@ void setup()
   //TODO - temp init, to be removed when Niv fixes carrier menu
   setRanges(72);
 
+  timer.every(HOUR_IN_MILIS, checkAndAlertValues);
   
   CARRIER_CASE = false; //TODO - to be changed when done
   carrier.begin();
 }
 
 void loop() 
-{ // TODO - decide if we should use delay (and freeze everything) of 1 hour
-  // or simply use Millis() and do the sampeling every hour inside an if statement
+{ 
+  timer.tick();
   ArduinoCloud.update();
-  if (carrier.Light.colorAvailable()) {
+
+  delay(1000);
+}
+
+void resetVars()
+/** Restes all variables used for calculating daily stats **/
+{
+  counter = 0;
+  totalLight = 0;
+  totalHumidity = 0;
+  totalTemperature = 0;
+}
+
+
+bool checkAndAlertValues(void *)
+/** This method is called every 1 hour - sample data, run calculation and alert/ update if needed**/
+{
+  sampleData();
+  
+  // content of the if- else should be changed to the actual action will do
+  if(moistPrecentage > maxMoist)
+  {
+    Serial.print(moistPrecentage);
+    Serial.println("\% - Too much water bro!");
+  }
+  else if(moistPrecentage < minMoist)
+  {
+    Serial.print(moistPrecentage);
+    Serial.println("\% - Give me some water ASAP!");
+  }
+  
+  updateAverageStats();
+  
+  return true; // needed for the timer thingy
+}
+
+void sampleData()
+/** Samples data from sensors It is out to alow uses from different locations**/
+{
+  if (carrier.Light.colorAvailable())
+  {
     int none;//not gonna be used
     carrier.Light.readColor(none,  none,  none, light);
   }
@@ -75,11 +116,11 @@ void loop()
 
   int rawMoistValue = analogRead(MoisterPin);
   moistPrecentage = map(rawMoistValue, AirValue, MoisterValue, 0, 100);
+}
 
-  checkValues(); //checks if values in range - for now only alerts in serial
-
-  //Maybe add some indicator on the screen of device (colors) that will illustrate how thirsty
-
+void updateAverageStats()
+/** Update avarege states**/
+{
   if (counter < 24)
   {
     totalLight += light;
@@ -94,32 +135,6 @@ void loop()
     averageHumidity = totalHumidity / counter;
     resetVars();
     //TODO - send some alert if not enough light in the last day?
-  }
-
-  
-  delay(5000);
-}
-
-void resetVars()
-{
-  counter = 0;
-  totalLight = 0;
-  totalHumidity = 0;
-  totalTemperature = 0;
-}
-
-
-void checkValues()
-{
-  if(moistPrecentage > maxMoist)
-  {
-    Serial.print(moistPrecentage);
-    Serial.println("\% - Too much water bro!");
-  }
-  else if(moistPrecentage < minMoist)
-  {
-    Serial.print(moistPrecentage);
-    Serial.println("\% - Give me some water ASAP!");
   }
 }
 
@@ -136,6 +151,7 @@ void onAverageHumidityChange() {
 }
 
 void setRanges(int carrier_input)
+/** set the ranges of required light & soil moister **/
 {
   int res = carrier_input & 15;
   switch(res)
